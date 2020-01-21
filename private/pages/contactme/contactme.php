5a0426b2b871352cef8ea27866c7dc6ab9c7c0cf
Charbel Kassis <charbel.kassis.90@gmail.com>
2020-01-21 00:36:34 -0500

    added cards to css

--- private/pages/contactme/contactme.php
+++ private/pages/contactme/contactme.php
@@ -2,19 +2,21 @@
 
 <p id="contactmeParagraph"><em>For any business and work related discussions you can contact me on my linkedIn profile, my email and my phone. I will reply to you as soon as possible!</em></p>
 
-<div id="contact">
-    <div>
-    <p id="linkedIn"><a href="https://www.linkedin.com/in/charbel-kassis">linkedIn</a></p>
-    <i class="fab fa-linkedin-in main-color center"></i>
-    </div>
+<div class="cards">
+    <div id="contact" class="card">
+        <div>
+        <p id="linkedIn"><a href="https://www.linkedin.com/in/charbel-kassis">linkedIn</a></p>
+        <i class="fab fa-linkedin-in main-color center"></i>
+        </div>
 
-    <div>
-    <p id="email"><a href="mailto:charbel.kassis.90@gmail.com">Email</a></p>
-    <i class="fas fa-envelope main-color center"></i>
-    </div>
+        <div>
+        <p id="email"><a href="mailto:charbel.kassis.90@gmail.com">Email</a></p>
+        <i class="fas fa-envelope main-color center"></i>
+        </div>
 
-    <div>
-    <p id="phone"><em>514-692-7310</em></p>
-    <i class="fas fa-mobile-alt main-color center"></i> 
+        <div>
+        <p id="phone"><em>514-692-7310</em></p>
+        <i class="fas fa-mobile-alt main-color center"></i> 
+        </div>
     </div>
 </div>
--- private/pages/projects/projects.php
+++ private/pages/projects/projects.php
@@ -1,30 +1,30 @@
 <h1 class="text-center">Projects</h1>
 <p class="text-center"><em>Check out the projects I have created. Hover over or tap the card to gain access to the links.</em></p>
-<div class="projects">
-    <div class="project">
+<div class="cards flipcards projects">
+    <div class="card flipcard project">
         <div class="front">
             <h2>University Projects</h2>
             <p>From simple websites to large software projects, I have placed all my university projects that I have access to in my Git Repository.</p>
         </div>
-        <div class="back center">
+        <div class="back block-center">
             <a href="https://github.com/CharbelKassis?tab=repositories">Look up my projects!</a>
         </div>
     </div>
-    <div class="project">
+    <div class="card flipcard project">
         <div class="front">
             <h2>Online Dictionary</h2>
             <p>This is a personal project. An online dictionary app that shows the definition of words based of a selected dictionary. If definitions are found, they will appear inside a textarea element. At the very bottom will appear a button that will toggle on a fullscreen image slider. The definitions are scraped from their respective dictionary websites, the images are fetched with the pixabay api.</p>
         </div>
-        <div class="back center">
+        <div class="back block-center">
             <a href="/dictionary/">Use it now!</a>
         </div>
     </div> 
-    <div class="project">
+    <div class="card flipcard project">
         <div class="front">
             <h2>Return On Ad Spend</h2>
             <p>This is a project I've created for Webtmize. Its purpose is to calculate the ad expenditures and profit when buying ad clicks on platforms such as Google Ads. The graph was generated thanks to the chartjs library. The app is integrated into the Wordpress environment.
         </div>
-        <div class="back center">
+        <div class="back block-center">
             <a href="https://www.webtmize.com/en/tools/advanced-roas-and-profit-calculator/">Try the calculator!</a>
         </div>
     </div>
