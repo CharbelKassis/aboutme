e22a206a6ad4dc823da655b24c8612379d4abf39
Charbel Kassis <charbel.kassis.90@gmail.com>
2020-01-21 01:45:07 -0500

    added more projects

--- private/pages/projects/projects.php
+++ private/pages/projects/projects.php
@@ -1,31 +1,51 @@
 <h1 class="text-center">Projects</h1>
 <p class="text-center"><em>Check out the projects I have created. Hover over or tap the card to gain access to the links.</em></p>
-<div class="cards flipcards projects">
-    <div class="card flipcard project">
-        <div class="front">
-            <h2>University Projects</h2>
-            <p>From simple websites to large software projects, I have placed all my university projects that I have access to in my Git Repository.</p>
+<div class="block-center">
+    <div class="cards flipcards projects">
+        <div class="card flipcard project">
+            <div class="front">
+                <h2>University Projects</h2>
+                <p>From simple websites to large software projects, I have placed all my university projects that I have access to in my Git Repository.</p>
+            </div>
+            <div class="back block-center">
+                <a href="https://github.com/CharbelKassis?tab=repositories">Look up my projects!</a>
+            </div>
         </div>
-        <div class="back block-center">
-            <a href="https://github.com/CharbelKassis?tab=repositories">Look up my projects!</a>
+        <div class="card flipcard project">
+            <div class="front">
+                <h2>Online Dictionary</h2>
+                <p>This is a personal project. An online dictionary app that shows the definition of words based of a selected dictionary. If definitions are found, they will appear inside a textarea element. At the very bottom will appear a button that will toggle on a fullscreen image slider. The definitions are scraped from their respective dictionary websites, the images are fetched with the pixabay api.</p>
+            </div>
+            <div class="back block-center">
+                <a href="/dictionary/">Use it now!</a>
+            </div>
+        </div> 
+        <div class="card flipcard project">
+            <div class="front">
+                <h2>Return On Ad Spend</h2>
+                <p>This is a project I've created for Webtmize. Its purpose is to calculate the ad expenditures and profit when buying ad clicks on platforms such as Google Ads. The graph was generated thanks to the chartjs library. The app is integrated into the Wordpress environment.
+            </div>
+            <div class="back block-center">
+                <a href="https://www.webtmize.com/en/tools/advanced-roas-and-profit-calculator/">Try the calculator!</a>
+            </div>
         </div>
-    </div>
-    <div class="card flipcard project">
-        <div class="front">
-            <h2>Online Dictionary</h2>
-            <p>This is a personal project. An online dictionary app that shows the definition of words based of a selected dictionary. If definitions are found, they will appear inside a textarea element. At the very bottom will appear a button that will toggle on a fullscreen image slider. The definitions are scraped from their respective dictionary websites, the images are fetched with the pixabay api.</p>
+        <div class="card flipcard project">
+            <div class="front">
+                <h2>Linklimousines</h2>
+                <p>Linklimouses's website is one of the website's I have worked on at Webtmize. The site was ported to Wordpress and was slightly modified from it's older version, some JavaScript functionalities have to be rewritten from scratch because they didn't exist in the Wordpress theme we have used.</p>
+            </div>
+            <div class="back block-center">
+                <a href="https://linklimousines.com/">View the website!</a>
+            </div>
         </div>
-        <div class="back block-center">
-            <a href="/dictionary/">Use it now!</a>
-        </div>
-    </div> 
-    <div class="card flipcard project">
-        <div class="front">
-            <h2>Return On Ad Spend</h2>
-            <p>This is a project I've created for Webtmize. Its purpose is to calculate the ad expenditures and profit when buying ad clicks on platforms such as Google Ads. The graph was generated thanks to the chartjs library. The app is integrated into the Wordpress environment.
-        </div>
-        <div class="back block-center">
-            <a href="https://www.webtmize.com/en/tools/advanced-roas-and-profit-calculator/">Try the calculator!</a>
+        <div class="card flipcard project">
+            <div class="front">
+                <h2>Services Financiers Flauto</h2>
+                <p>Servicefinancierflauto is a website that I have created with the help of Webtmize's graphic designer at Webtmize.</p>
+            </div>
+            <div class="back block-center">
+                <a href="https://servicesfinanciersflauto.com/">View the website!</a>
+            </div>
         </div>
     </div>
 </div>
