7dd9258024e4e999b08341c022933cf513e85b89
Charbel Kassis <charbel.kassis.90@gmail.com>
2020-05-14 11:01:13 -0400

    cors proxy check if url starts with http(s)

--- private/post/dictionary/index.php
+++ private/post/dictionary/index.php
@@ -1,5 +1,11 @@
 <?php
     $q = $_GET["q"];
+    preg_match('/^(http(?:s)?\:\/\/).*/',$q,$matches);
+
+    //make sure the query starts with http or https to prevent file_get_content from searching local files
+    if($matches[1] !== "https://" && $matches[1] !== "http://")
+        $q = "http://".$q;
+
     $html = file_get_contents($q);
     echo($html);
 ?>
